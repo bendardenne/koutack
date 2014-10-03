e588d8719455f6be99f2544a691aa21d9d3a6c40
Benoît Dardenne <benoit.dardenne@gmail.com>
2014-10-03 15:18:46 +0200

    init + state.__str__ + gitignore + ...

--- koutack.py
+++ koutack.py
@@ -3,27 +3,69 @@
 from search import *
 
 
+######################  State class  ###############################
+
+class State :
+
+    def __init__(self):
+        self.array = []
+    
+    def appendRow(self, row):
+        self.array.append(row)
+
+    def isSolution(self):
+        empty = True
+        for row in self.array:
+            for tile in row:
+                if tile:
+                    if not empty:
+                        return False
+                    empty = False
+        return  True
+
+    def __str__(self) :
+        string = ""
+        for row in self.array:
+            for tile in row:
+                if tile == ():
+                    string += '.'
+                else: 
+                    string += (tile[0] if len(tile) == 1 else "[" + ",".join(tile) + "]")
+                string += ' '
+            string = string[:-1] + '\n'
+        return string        
+         
 ######################  Implement the search #######################
 
 class Koutack(Problem):
 
     def __init__(self,init):
-
+        self.initial = State()
+        f = open(init, 'r')
+        line = []
+        for line in f:
+            row = []
+            for tile in line.split():
+                if (tile == "."):
+                    row.append(())
+                else:
+                    row.append((tile[0],))
+            self.initial.appendRow(row)
+        print(self.initial.isSolution()) 
+        
     
     def goal_test(self, state):
-        pass
+        return state.isSolution()
 
-        
+
     def successor(self, state):
         pass
         
 
-
-
-
 ###################### Launch the search #########################
-    
 problem=Koutack(sys.argv[1])
+
+"""    
 #example of bfs search
 node=breadth_first_graph_search(problem)
 #example of print
@@ -32,4 +74,4 @@ path.reverse()
 for n in path:
     print(n.state) #assuming that the __str__ function of states output the correct format
 
-        
+   """     
